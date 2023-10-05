1007a07573c66390fd46480d1460b096a8f66d24
allenkays <allenkays@tutanota.com>
2023-10-05 14:45:23 +0300

    Refactor to use face_recognition module in place of cv2

--- app/face_detection.py
+++ app/face_detection.py
@@ -1,42 +1,26 @@
-#!/usr/bin/env python3
-"""
-This is a class containing methods to detect faces in a
-photo or video
-"""
-
-# pylint:disable=no-member
-import cv2
-import os
+import face_recognition
 
 
 class FaceDetector:
     """
-    Class for detecting faces in an image using OpenCV's Haar cascade.
+    Class for detecting faces in an image using the face_recognition library.
     """
-    def __init__(self, cascade_path):
-        """
-        Initializes the FaceDetector with the given Haar cascade xml file
+    def __init__(self):
+        pass
 
-        Args:
-            cascade_path (str): Path to haar cascade file
-        """
-        self.face_cascade = cv2.CascadeClassifier(cascade_path)
-
-    def detect_faces(self, image):
+    def detect_faces(self, image_path):
         """
         Detects faces in the given image.
 
         Args:
-            image (numpy.ndarray): The image to detect faces from
+            image_path (str): Path to the image file.
 
         Returns:
-            List of tuples: Bounding box coordinates of the detected faces
+            List of tuples: Bounding box coordinates of the detected faces.
         """
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(
-            gray, scaleFactor=1.1, minNeighbors=7, minSize=(30, 30)
-        )
-        return faces
+        image = face_recognition.load_image_file(image_path)
+        face_locations = face_recognition.face_locations(image)
+        return face_locations
 
     def detect_faces_in_video(self, video_path):
         """
@@ -63,40 +47,25 @@ class FaceDetector:
                 break  # End of the video
 
             # Detect faces in the current frame
-            faces = self.detect_faces(frame)
+            face_locations = self.detect_faces_in_frame(frame)
 
             # Append the detected faces to the result list
-            detected_faces_in_video.append(faces)
+            detected_faces_in_video.append(face_locations)
 
         # Release the video capture object
         cap.release()
 
         return detected_faces_in_video
 
+    def detect_faces_in_frame(self, frame):
+        """
+        Detects faces in a single frame.
 
-"""
-if __name__ == '__main__':
-    img = cv2.imread('group 2.jpg')
+        Args:
+            frame (numpy.ndarray): The frame to detect faces from.
 
-    # Get the absolute path to the XML file
-    cascade_path = (
-        '/home/eq/Documents/Projects_Ex/Face_X_Final/data/haar_face.xml'
-    )
-
-    # Initialize the FaceDetector with the absolute file path
-    detector = FaceDetector(cascade_path)
-
-    # Detect faces in the image
-    detected_faces = detector.detect_faces(img)
-
-    # Output number of detected faces
-    print(f'Number of faces found = {len(detected_faces)}')
-
-    # draw rectangle around detected faces
-    for (x,y,w,h) in detected_faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
-
-    cv2.imshow('Detected Faces', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-"""
+        Returns:
+            List of tuples: Bounding box coordinates of the detected faces.
+        """
+        face_locations = face_recognition.face_locations(frame)
+        return face_locations
